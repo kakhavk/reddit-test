f6a14de3debe57156c578aefeb41490abd4bcb6e
kakhaber <kakhavk@gmail.com>
2016-10-17 00:24:54 +0400

    result ok = -1

--- app/src/main/java/com/example/mac10/reddittest/InfoActivity.java
+++ app/src/main/java/com/example/mac10/reddittest/InfoActivity.java
@@ -5,51 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
 
 public class InfoActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView info1view, info2view, info3view;
-    private LinearLayout info1layout, info2layout, info3layout;
+
+    private LinearLayout lr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-    }
+        LinearLayout rl=(LinearLayout) findViewById(R.id.lr1);
 
-    private void findViews(){
-        info1view=(TextView) findViewById(R.id.info1_name);
-        info2view=(TextView) findViewById(R.id.info2_name);
-        info3view=(TextView) findViewById(R.id.info3_name);
-
-        info1layout=(LinearLayout) findViewById(R.id.info1_layout);
-        info2layout=(LinearLayout) findViewById(R.id.info2_layout);
-        info3layout=(LinearLayout) findViewById(R.id.info3_layout);
-
-        info1layout.setOnClickListener(this);
-        info2layout.setOnClickListener(this);
-        info3layout.setOnClickListener(this);
+        lr=(LinearLayout) findViewById(R.id.lr1);
+        lr.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         Intent intent=new Intent();
-        String info;
-        int imageId;
 
+        setResult(RESULT_OK, intent);
+        finish();
+    }
 
-            if(v==info1layout) {
-                info = info1view.getText().toString();
-                imageId = R.mipmap.bu;
-            }else if(v==info1layout) {
-                info = info2view.getText().toString();
-                imageId = R.mipmap.cikvi;
-            }else if(v==info1layout) {
-                info = info3view.getText().toString();
-                imageId = R.mipmap.tevzi;
-            }
-
+    public void onCancel(View view){
+        setResult(RESULT_CANCELED);
+        finish();
     }
 }
--- app/src/main/java/com/example/mac10/reddittest/MainActivity.java
+++ app/src/main/java/com/example/mac10/reddittest/MainActivity.java
@@ -5,53 +5,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button chooseInfoButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
-
-
-/*
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Integer clickCount=0;
-    int[] colorLabels=new int[4];
+    private static final int INFO_REQUEST_CODE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView=(TextView) findViewById(R.id.textView1);
-        Button button=(Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
+        chooseInfoButton=(Button) findViewById(R.id.choose_info_button);
+
+
+        chooseInfoButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                ;
-                colorLabels[0]=ContextCompat.getColor(MainActivity.this, R.color.colorAccent);
-                colorLabels[1]=ContextCompat.getColor(MainActivity.this, R.color.colorGreen);
-                colorLabels[2]=ContextCompat.getColor(MainActivity.this, R.color.colorPrimary);
-                colorLabels[3]=ContextCompat.getColor(MainActivity.this, R.color.colorPrimaryDark);
-                Random r=new Random();
-
-                textView.setTextColor(colorLabels[r.nextInt(4)]);
-                clickCount+=1;
-                textView.setText("HIIIIIII"+clickCount);
+            public void onClick(View v){
+                startActivityForResult(new Intent(MainActivity.this, InfoActivity.class), INFO_REQUEST_CODE);
             }
         });
-
     }
-
 
     @Override
-    public void onClick(View v) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
 
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==INFO_REQUEST_CODE){
+            Toast.makeText(this, " INFO_REQUEST_CODE CODE="+INFO_REQUEST_CODE, Toast.LENGTH_SHORT).show();
+            if(resultCode==RESULT_OK){
+                Toast.makeText(this, "RESULT_OK="+RESULT_OK, Toast.LENGTH_LONG).show();
+            }
+        }else{
+            Toast.makeText(this, "INFO REQUEST SAD ARIIIS", Toast.LENGTH_SHORT).show();
+        }
     }
 }
-*/
+
