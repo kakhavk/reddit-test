49c329a22ee6c182c8b0b92682a1ac7c5fa11ece
kakhaber <kakhavk@gmail.com>
2016-10-20 00:13:01 +0400

    thread get content from internet

--- app/src/main/java/com/example/mac10/reddittest/MainActivity.java
+++ app/src/main/java/com/example/mac10/reddittest/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.mac10.reddittest;
 
+import java.io.InputStream;
+import java.net.URL;
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button chooseInfoButton;
+    private ImageView testimageview1;
 
     private static final int INFO_REQUEST_CODE=1;
 
@@ -20,14 +27,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         chooseInfoButton=(Button) findViewById(R.id.choose_info_button);
-
+        testimageview1=(ImageView) findViewById(R.id.testimageview1);
 
         chooseInfoButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                startActivityForResult(new Intent(MainActivity.this, InfoActivity.class), INFO_REQUEST_CODE);
+                //startActivityForResult(new Intent(MainActivity.this, InfoActivity.class), INFO_REQUEST_CODE);
+                new Thread(new Runnable(){
+                    //@Override
+                    public void run() {
+                        final Bitmap bitmap=loadImageFromNetwork("http://xtreme.hiutils.org/images/logo/xtremetitle.jpg");
+                        testimageview1.post(new Runnable() {
+                            //@Override
+                            public void run() {
+                                testimageview1.setImageBitmap(bitmap);
+                            }
+                        });
+                    }
+                }).start();
             }
         });
+    }
+
+    private Bitmap loadImageFromNetwork(String url){
+        try{
+            Bitmap bitmap=BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
+            return bitmap;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
